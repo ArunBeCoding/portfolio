b5ac89472e42d0c0e79eed283df13462a332888a
ArunBeCoding <arunkumarr97@gmail.com>
2020-07-11 19:04:12 +0800

    Updates

---
+++ precache-manifest.e9c6cf4dc2c9f45229abf7915a06cbea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679cbb3d86e8629f4abd5759e536c72d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a46eedd93c59acdfba1",
+    "url": "/portfolio/static/css/2.7f83aeb6.chunk.css"
+  },
+  {
+    "revision": "8e1525d183561239c0ef",
+    "url": "/portfolio/static/css/main.424b3e1b.chunk.css"
+  },
+  {
+    "revision": "7a46eedd93c59acdfba1",
+    "url": "/portfolio/static/js/2.d03523ac.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/portfolio/static/js/2.d03523ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1525d183561239c0ef",
+    "url": "/portfolio/static/js/main.6c14743c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "af0e62427c707a7a5b990097b441d9fb",
+    "url": "/portfolio/static/media/Catena.af0e6242.jpg"
+  },
+  {
+    "revision": "4b5b6ac13222c081975f9693b66f3573",
+    "url": "/portfolio/static/media/Cloud.4b5b6ac1.jpg"
+  },
+  {
+    "revision": "4acec26ef12796000b73799fc26414d8",
+    "url": "/portfolio/static/media/Invest_logo.4acec26e.jpg"
+  },
+  {
+    "revision": "651a7d68d85d911723d2ceb5e5e24a5d",
+    "url": "/portfolio/static/media/Profile_Pic.651a7d68.jpg"
+  },
+  {
+    "revision": "cd311cee2f9d61644bd258c951d81421",
+    "url": "/portfolio/static/media/header-background.cd311cee.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2c072f38f8b9efbbb2b28067574d3cf6.js"
+  "/portfolio/precache-manifest.e9c6cf4dc2c9f45229abf7915a06cbea.js"
 );
 
 self.addEventListener('message', (event) => {
