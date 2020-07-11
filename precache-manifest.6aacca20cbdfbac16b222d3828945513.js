ff80e1c7890a022d0709d775d3f77a9c7461b3b6
ArunBeCoding <arunkumarr97@gmail.com>
2020-07-12 02:37:46 +0800

    Updates

--- precache-manifest.6aacca20cbdfbac16b222d3828945513.js
+++ precache-manifest.6aacca20cbdfbac16b222d3828945513.js
@@ -1,31 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f89734c405418d7e0aa546aff9466c2",
+    "revision": "de3ff4f7eacb1ccf42be99d2f80ac2e2",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "7a46eedd93c59acdfba1",
+    "revision": "616384fceb3778d6afbb",
     "url": "/portfolio/static/css/2.7f83aeb6.chunk.css"
   },
   {
-    "revision": "c13e3349e2cb22678efc",
-    "url": "/portfolio/static/css/main.c2451d8a.chunk.css"
+    "revision": "858e8beff284de2c45f8",
+    "url": "/portfolio/static/css/main.99811773.chunk.css"
   },
   {
-    "revision": "7a46eedd93c59acdfba1",
-    "url": "/portfolio/static/js/2.d03523ac.chunk.js"
+    "revision": "616384fceb3778d6afbb",
+    "url": "/portfolio/static/js/2.36185fec.chunk.js"
   },
   {
     "revision": "0042ed35eedf85f73c564b78f2a06746",
-    "url": "/portfolio/static/js/2.d03523ac.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.36185fec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c13e3349e2cb22678efc",
-    "url": "/portfolio/static/js/main.57ba3ef3.chunk.js"
+    "revision": "858e8beff284de2c45f8",
+    "url": "/portfolio/static/js/main.9d6c3180.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
     "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "eb4f555a554bbe7a06e47b862aa1029f",
+    "url": "/portfolio/static/media/CPFHackathon.eb4f555a.png"
   },
   {
     "revision": "af0e62427c707a7a5b990097b441d9fb",
@@ -36,12 +40,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/Cloud.4b5b6ac1.jpg"
   },
   {
+    "revision": "aaf5a57c0e4a8733871359c66942f634",
+    "url": "/portfolio/static/media/Invest.aaf5a57c.jpg"
+  },
+  {
     "revision": "4acec26ef12796000b73799fc26414d8",
     "url": "/portfolio/static/media/Invest_logo.4acec26e.jpg"
   },
   {
+    "revision": "cb2eb25e9c96f06e1dcad37d5de89f22",
+    "url": "/portfolio/static/media/Makerthon.cb2eb25e.jpg"
+  },
+  {
     "revision": "651a7d68d85d911723d2ceb5e5e24a5d",
     "url": "/portfolio/static/media/Profile_Pic.651a7d68.jpg"
+  },
+  {
+    "revision": "f9e0012b452efdf485021488791c5808",
+    "url": "/portfolio/static/media/SoftEng.f9e0012b.jpg"
   },
   {
     "revision": "cd311cee2f9d61644bd258c951d81421",
