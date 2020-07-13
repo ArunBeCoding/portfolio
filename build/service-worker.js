bedf9b7a66787015d6e0cea5452498a1ca6841b2
ArunBeCoding <arunkumarr97@gmail.com>
2020-07-13 10:58:29 +0800

    adjusted gif mobile size

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ffc7509d71fec52f251364921f44625e.js"
+  "/portfolio/precache-manifest.c67f01ec4a48cc69fbce88615767e0f6.js"
 );
 
 self.addEventListener('message', (event) => {
