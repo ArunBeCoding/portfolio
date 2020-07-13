1db9c59c1ce8191ef00e31bd5f277693c5bd298c
ArunBeCoding <arunkumarr97@gmail.com>
2020-07-14 01:16:02 +0800

    added favicon

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.182854d4edf12c38d73db6d491fec616.js"
+  "/portfolio/precache-manifest.2cb693eeb4f8f01d42b3cc42a356ab91.js"
 );
 
 self.addEventListener('message', (event) => {
