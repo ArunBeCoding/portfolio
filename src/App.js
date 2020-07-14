f0e670a3236553beb2d7b614000f9901b28c7d92
ArunBeCoding <arunkumarr97@gmail.com>
2020-07-14 15:57:19 +0800

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import './css/App.css';
 import Header from './Header.js';
-import Body from './Body.js';
 import Footer from './Footer.js';
+// import Body from './Body.js';
+import About from "./Content/About";
+import Internship from "./Content/Internship";
+import Projects from "./Content/Projects";
 
 
 function App() {
   return (
     <div className="Background">
-      <Header/>
-      <Body/>
-      <Footer/>
+      <div id="ArunKumarr">
+        <Header/>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#ArunKumarr"  className="MyName">Arun Kumarr</a></li>
+          {/* <li><a href="#Work">Work</a></li> */}
+          <li><a href="#AboutMe">About</a></li>
+          <li><a href="#Internships">Internships</a></li>
+          <li><a href="#Projects">Projects</a></li>
+          <li><a href="#Contact">Contact</a></li>
+        </ul>
+      </nav>
+      {/* <div id="Work">
+        <Body />
+      </div> */}
+      <div className="Body">
+        <div id="AboutMe">
+          <About />
+        </div>
+        <div id="Internships">
+          <Internship />
+        </div> 
+        <div id="Projects">
+        <Projects />
+        </div>    
+      </div>
+      <div id="Contact">
+        <Footer/>
+      </div>
     </div>
   );
 }
